07aa22d6033294b2a2989a6150f4bc932fd7f568
joseph-cavarretta <josephmc241@gmail.com>
2024-09-16 19:00:35 -0600

    clean up to queries

--- src/queries.sql
+++ src/queries.sql
@@ -6,7 +6,8 @@ WITH author AS (
     WHERE id = '< insert id here >'
 )
 SELECT
-    first_publish_year, COUNT(*)
+    first_publish_year, 
+    COUNT(*) AS total_books_published
 FROM author a 
     JOIN books b ON a.id=b.author_id
 GROUP BY first_publish_year
@@ -20,12 +21,13 @@ WITH author AS (
     WHERE id = '< insert id here >'
 )
 SELECT
+    a.id,
     ROUND(
         COUNT(*) / COUNT(DISTINCT first_publish_year)
     , 2) AS avg_books_published_per_year
 FROM author a 
     JOIN books b ON a.id=b.author_id
-;
+GROUP BY a.id;
 
 -- books published by year and total books published by each author
 SELECT
